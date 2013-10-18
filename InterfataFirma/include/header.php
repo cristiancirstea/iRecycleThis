970a03863cbdbc9eef6ab42b7af9d719bbbc5f05
w01f6 <cristiancocioaba@gmail.com>
2013-10-18 23:37:43 +0300

    Includes

---
+++ InterfataFirma/include/header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <link rel ="stylesheet" type = "text/css" href = "library/css/bootstrap.css">
+<link rel ="stylesheet" type = "text/css" href = "library/css/my_CSS.css">
+<script type = "text/javascript" src = "library/js/bootstrap.js"></script>
+<script type = "text/javascript" src = "library/js/my_JS.js"></script>
+<title> Test </title>
+</head>
+
+<body>
+
